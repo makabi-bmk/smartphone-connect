4a53ae268dddad7d53d08cdf926be6a287f366c4
makabi <mi161332@edu.okinawa-ct.ac.jp>
2020-10-13 21:49:36 +0900

    デバイスの方向と加速度を検出するコードを書いた

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const con = new WebSocket('ws://localhost:8081/');
 var ID;
 
+/*
 try {
     
     con.onopen = function() {
@@ -18,5 +19,18 @@ con.onmessage = function(msg) {
     alert(msg.data);
     console.log(msg.data);
 };
+*/
 
+// デバイスの方向の変化を検出したとき
+window.addEventListener('deviceorientation', function(e) {
+    console.log(e.alpha); // z軸 0 〜 360
+    console.log(e.beta);  // x軸 -180 〜 180
+    console.log(e.gamma); // y軸 -90 〜 90
+});
 
+// デバイスの加速度の変化を検出したとき
+  window.addEventListener('devicemotion', function(e) {
+    console.log(e.acceleration.x); // x軸
+    console.log(e.acceleration.y); // y軸
+    console.log(e.acceleration.z); // z軸
+});
