bc1f9915117624d09fb43973fe3600f30e20bfb3
makabi <mi161332@edu.okinawa-ct.ac.jp>
2020-10-13 14:12:04 +0900

    通信の処理を書いた

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 window.onload = function() {
-    console.log("hello");
+    try {
+        const con = new WebSocket('ws://localhost:8081/');
+        
+        con.onopen = function() {
+            console.log('coを開始しました');
+            con.send('Hello WebSocket!');
+        };
+
+        con.onmessage = function(msg) {
+            alert(msg.data);
+            console.log(msg.data);
+        };
+        // con.close();
+
+    } catch (error) {
+        console.log(error);
+    }
 }
