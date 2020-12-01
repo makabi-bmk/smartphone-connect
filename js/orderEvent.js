ec1d2bc64c2c68cc74138909f484ef7a5c2c2604
makabi <mi161332@edu.okinawa-ct.ac.jp>
2020-12-02 02:20:48 +0900

    画像のURL間違っていたので直した

--- js/orderEvent.js
+++ js/orderEvent.js
@@ -2,12 +2,12 @@ const { url } = require("inspector");
 
 //画像を変更する
 function changeImage(image_num) {
-    document.getElementById("image").src = "../img/image" + image_num + ".png";
+    document.getElementById("image").src = "../image/image" + image_num + ".png";
 }
 
 //背景画像を変更する
 function changeBackImage(image_num) {
-    document.getElementById("image").src = "../back_img/back_image" + image_num + ".png";
+    document.getElementById("image").src = "../back_image/back_image" + image_num + ".png";
 }
 
 //画像の大きさを変更する(px)
