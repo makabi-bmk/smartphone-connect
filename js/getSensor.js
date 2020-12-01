9652a9215bc51300bd9d0058f37b6bb1cc9f108a
makabi <mi161332@edu.okinawa-ct.ac.jp>
2020-12-02 00:09:15 +0900

    画面をタッチしたときの処理を変えた

--- js/getSensor.js
+++ js/getSensor.js
@@ -33,8 +33,7 @@ window.addEventListener('devicemotion', function(e) {
 });
 
 //画面をタッチしたとき
-var image = document.getElementById("body");
-image.addEventListener("touchstart", function(event) {
+window.addEventListener("touchstart", function(event) {
     data.tap_x = event.changedTouches[0].pageX;
     data.tap_y = event.changedTouches[0].pageY;
 }, false);
