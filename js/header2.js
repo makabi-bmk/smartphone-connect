2ab02dc5abdc067d498e2c6b0d97b619c410b282
makabi <mi161332@edu.okinawa-ct.ac.jp>
2020-12-08 02:45:54 +0900

    require使えなかった…

---
+++ js/header2.js
@@ -0,0 +1,40 @@
+
+var sensorData = {
+    smartphone_ID : 0,
+    scratch_ID : 0,
+    type : 0,
+    request_num : 0,
+    alpha : 0,
+    beta : 0,
+    gamma : 0,
+    acceleration_x : 0,
+    acceleration_y : 0,
+    acceleration_z : 0,
+    image_num  : 0,
+    voice_message : "",
+    tap_x : 0,
+    tap_y : 0,
+    image_touch : false,
+    swipe_vertical : false,
+    swipe_horizontal : false
+};
+
+var orderData = {
+    request_num : 0,
+    smartphone_ID : 0,
+    scratch_ID : 0,
+    type : 1,
+    flag : 0,
+    response_num : 0,
+    image_num : 0,
+    back_image_num : 0,
+    size : 100,
+    pos_x : 0,
+    pos_y : 0,
+    message : "",
+    alert_message : "",
+    bgm_num : 0,
+    pos_x : 0,
+    pos_y : 0,
+    size : 0
+};
