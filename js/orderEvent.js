38ed60b2cbd49fcad63093b97a801f60c4a82af1
makabi <mi161332@edu.okinawa-ct.ac.jp>
2021-01-16 18:12:44 +0900

    ボタンをaタグにした

--- js/orderEvent.js
+++ js/orderEvent.js
@@ -20,7 +20,7 @@ function setInputText() {
 function changeButtonText(text) {
     console.log("ボタンの文字:" + text);
     if (text.length < MAX_BUTTON_TEXT_LENGTH) {
-        document.getElementById("button").value = text;
+        document.getElementById("button").setAttribute("value", text);
     }
 }
 
