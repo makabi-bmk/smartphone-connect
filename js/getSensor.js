84e96022a6a2aefb92e1db6dd6e508e0a2f46b5c
makabi <mi161332@edu.okinawa-ct.ac.jp>
2021-01-16 19:24:00 +0900

    スワイプにログ描いた

--- js/getSensor.js
+++ js/getSensor.js
@@ -4,18 +4,11 @@
 //     sensorData.image_touch = true;
 // }
 
-// $('#button').click(function(evt){
-//     console.log("オブジェクトが押された");
-//     sensorData.image_touch = true;
-//     //location.href = location.href + '#h';
-//     return false;
-// });
-
 $('document').on('click', '#button', function(evt) {
     // メニューを開いたり閉じたりする処理
     console.log("オブジェクトが押された");
     sensorData.image_touch = true;
-  });
+});
 
 // // オブジェクトの位置を検知する
 // function setImagePosition() {
@@ -47,6 +40,7 @@ var touchMoveY;
 
 // スワイプの開始(タップ)
 window.addEventListener("touchstart", function(event) {
+    console.log("さわった");
     event.preventDefault();
     // 座標の取得
     touchStartX = sensorData.tap_position_x = event.touches[0].pageX;
@@ -56,6 +50,7 @@ window.addEventListener("touchstart", function(event) {
 // タップの移動
 window.addEventListener("touchmove", function(event) {
     event.preventDefault();
+    console.log("うごいた");
     // 座標の取得
     touchMoveX = event.changedTouches[0].pageX;
     touchMoveY = event.changedTouches[0].pageY;
@@ -63,18 +58,23 @@ window.addEventListener("touchmove", function(event) {
 
 // タップの終了時(スワイプの判定)
 window.addEventListener("touchend", function(event) {
+    console.log("はなした");
     if (touchStartX > touchMoveX && touchStartX > (touchMoveX + 50)) {
         //右から左に指が移動した場合
         sensorData.swipe_horizontal = true;
+        console.log("右にスワイプ");
     } else if (touchStartX < touchMoveX && (touchStartX + 50) < touchMoveX) {
         //左から右に指が移動した場合
         sensorData.swipe_horizontal = true;
+        console.log("左にスワイプ");
     }
     if (touchStartY > touchMoveY && touchStartY > (touchMoveY + 50)) {
         //上から下に指が移動した場合
         sensorData.swipe_vertical = true;
+        console.log("下にスワイプ");
     } else if (touchStartY < touchMoveY && (touchStartY + 50) < touchMoveY) {
         //下から上に指が移動した場合
         sensorData.swipe_vertical = true;
+        console.log("上にスワイプ");
     }
 }, { passive: false });
