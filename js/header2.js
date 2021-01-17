7098ec8d006d0e41bd2bcb3d4b25d003fa77756c
makabi <mi161332@edu.okinawa-ct.ac.jp>
2021-01-17 21:04:41 +0900

    背景画像の大きさをcontainにした

--- js/header2.js
+++ js/header2.js
@@ -14,7 +14,7 @@ const MAX_MESSAGE_LENGTH = 100;
 // オブジェクトの大きさと角度の初期値
 const DEFAULT_SIZE = 60;
 const DEFAULT_ANGLE = 0;
-const DEFAULT_POS_X = screen.width / 2;
+const DEFAULT_POS_X = 0;
 const DEFAULT_POS_Y = screen.height / 2;
 
 const TYPE = {
